1f05c32cb00a84ef08bab34667b79f702a6218bd
Scheronlini <scheronlini@gmail.com>
2021-02-06 17:09:44 -0300

    Projeto Criado

---
+++ src/main/java/com/sfauser/cadastroVacinacao/CadastroVacinacaoApplication.java
@@ -0,0 +1,13 @@
+package com.sfauser.cadastroVacinacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroVacinacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroVacinacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sfauser/cadastroVacinacao/CadastroVacinacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfauser.cadastroVacinacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroVacinacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
