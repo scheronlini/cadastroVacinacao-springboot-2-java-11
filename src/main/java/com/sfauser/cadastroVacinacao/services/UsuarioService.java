ea60216d1701a7efb7b913a6207cb62c2240f28d
scheronlini <scheronlinigmail.com>
2021-02-07 18:24:57 -0300

    exeçoes 2

--- src/main/java/com/sfauser/cadastroVacinacao/services/UsuarioService.java
+++ src/main/java/com/sfauser/cadastroVacinacao/services/UsuarioService.java
@@ -4,35 +4,45 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.sfauser.cadastroVacinacao.entities.Usuario;
 import com.sfauser.cadastroVacinacao.repositories.UsuarioRepository;
+import com.sfauser.cadastroVacinacao.services.exceptions.DatabaseException;
 import com.sfauser.cadastroVacinacao.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UsuarioService {
-	
+
 	@Autowired
 	private UsuarioRepository repository;
-	
+
 	public List<Usuario> FindAll() {
 		return repository.findAll();
 	}
-	
+
 	public Usuario FindById(Long id) {
 		Optional<Usuario> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public Usuario insert(Usuario obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}	catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
-	
+
 	public Usuario update(Long id, Usuario userObj) {
 		Usuario databaseObj = repository.getOne(id);
 		updateData(databaseObj, userObj);
@@ -40,7 +50,7 @@ public class UsuarioService {
 	}
 
 	private void updateData(Usuario databaseObj, Usuario userObj) {
-		
+
 		if (userObj.getNome() != null) {
 			databaseObj.setNome(userObj.getNome());
 		}
